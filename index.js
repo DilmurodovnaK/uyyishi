3e0a13070926931473e71a6b2ee712f59661154a
DilmurodovnaK <yulchiyevakamola2006@gmail.com>
2023-09-24 21:55:23 +0300

    my first lesson

---
+++ index.js
@@ -0,0 +1,211 @@
+//  if else
+
+// 1-misol
+
+// a = 2;
+
+// if (a > 0) {
+//     console.log(
+//         'musbat son');
+// } else {
+//     console.log(
+//         'manfiy son');
+// }
+
+// // 2-misol
+
+// let a = 3;
+
+// if (a / 2 != 0) {
+//     console.log('toq son');
+// } else {
+//     console.log('juft son');
+// }
+
+// 3-misol
+
+// let a = 4;
+
+// if (a / 2 == 0) {
+//     console.log('juft son');
+// } else {
+//     console.log('toq son')
+// }
+
+// 4-misol
+
+// let a = 23
+// let b = 12
+
+// if (a > 0 && b > 0) {
+//     console.log("javob tog'ri");
+// } else {
+//     console.log("javob xato");
+// }
+
+// 5-misol
+
+// let a = 21;
+// let b = 19;
+// let c = 16;
+
+// if (a > b && b > c) {
+//     console.log("javob tog'ri");
+// } else {
+//     console.log("javob notog'ri");
+// }
+
+// 6-misol
+
+// let a = 6;
+// let b = 19;
+// let c = 21;
+
+// if(a < b && b < c){
+//     console.log("javob tog'ri");
+// }else{
+//     console.log("javob notog'ri");
+// }
+
+// 7-misol
+
+// let a = 6;
+// let b = 19;
+
+// if (a && b / 2 != 0) {
+//     console.log("toq sonlar...");
+// } else {
+//     console.log("xato javob, ular juft sonlar");
+// }
+
+// 8-misol
+
+// let a = 6;
+// let b = 19;
+// let c = 21;
+// let d = 13;
+// if (a >= 0 && b >= 0 && c >= 0 && d >= 0) {
+//     console.log('bu sonlar musbat sonlar');
+// } else {
+//     "bular manfiy sonlar"
+// }
+
+// 9-misol
+
+// let a = 6;
+// let b = -19;
+// let c = 21;
+
+// if(a >= 0  b >= 0  c >= 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// 10-misol
+
+// let kun = +prompt("hafta kunini kirgizing ");
+
+// if(kun == 1){
+//     console.log("dushanba");
+// }else if(kun == 2){
+//     console.log("seshanba");
+// }else if(kun == 3){
+//     console.log("chorshanba");
+// }else if(kun == 4){
+//     console.log("payshanba");
+// }else if(kun == 5){
+//     console.log("juma");
+// }else if(kun == 6){
+//     console.log("shanba");
+// }else if(kun == 7){
+//     console.log("yakshanba");
+// }else{
+// console.log("bunday kun yoq");
+// }
+
+// 11-misol
+
+// let nomer = +prompt("telefon raqamingiz kodini kiriting ");
+
+// if (nomer == 97) {
+//     console.log("mobi uz");
+// }else if (nomer == 99) {
+//     console.log("uzmobile");
+// }else if (nomer == 33) {
+//     console.log("humans");
+// }else if (nomer == 98) {
+//     console.log("ucel");
+// }else if (nomer == 90) {
+//     console.log("billine");
+// }else if (nomer == 91) {
+//     console.log("ucel-1");
+// }else if (nomer == 71) {
+//     console.log("uy telefoni");
+// }else{
+//     console.log("bunday kod yoq");
+// }
+
+// 12-misol
+
+// let son = 20;
+
+// if(son >= 0){
+//     son += 1
+// }else{
+//     son -= 1
+// }
+// console.log(son);
+
+// 13-misol
+
+// let son = 23;
+
+// if(son >= 0){
+//     son**3
+//     console.log(son);
+// }else{
+//     son / 2
+//     console.log(son);
+// }
+
+// 14-misol
+
+// let a = 12;
+// let b = 21;
+
+// if (a > b) {
+//     console.log(
+//         '12 katta');
+// } else {
+//     console.log(
+//         '21 katta');
+// }
+
+// 15-misol
+
+// let a = 5;
+// let b = 2;
+// let c = 19;
+
+// if (a > b && a > c) {
+//     console.log('5 katta');
+// } else if (b > a && b > c) {
+//     console.log('2 katta');
+// } else if (c > a && c > b) {
+//     console.log('19 katta');
+// }
+
+// 16-misol
+
+// let a = 20;
+// let b = 13;
+// let c = 8;
+
+// if (a < b && a < c) {
+//     console.log('20 kichkina');
+// } else if (b < a && b < c) {
+//     console.log('13 kichkina');
+// } else if (c < a && c < b) {
+//     console.log('8 kichkina');
+// }
